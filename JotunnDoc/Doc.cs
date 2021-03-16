98e0a9a5ce8bace00bd463cd88338e5d01a0a34a
Radu Schirliu <radu.schirliu@gmail.com>
2021-03-16 10:48:55 -0600

    feat: Added base for documentation mod

---
+++ JotunnDoc/Doc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JotunnDoc
+{
+    public abstract class Doc
+    {
+        
+    }
+}
---
+++ JotunnDoc/JotunnDoc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+using HarmonyLib;
+
+namespace JotunnDoc
+{
+    [BepInPlugin("com.bepinex.plugins.jotunndoc", "JotunnDoc", "0.1.0")]
+    [BepInDependency("com.bepinex.plugins.jotunnlib")]
+    public class JotunnDoc
+    {
+        private void Awake()
+        {
+
+        }
+    }
+}
