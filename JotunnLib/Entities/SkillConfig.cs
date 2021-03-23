df806f7c5aa6852b79ee71145a6f6a00a2a006d8
Ratik Kapoor <ratikrk@gmail.com>
2021-03-23 12:44:50 -0600

    feat: Added FromSkillInjector support for old skills

--- JotunnLib/Entities/SkillConfig.cs
+++ JotunnLib/Entities/SkillConfig.cs
@@ -14,7 +14,14 @@ namespace JotunnLib.Entities
             set
             {
                 this._identifier = value;
-                UID = (Skills.SkillType)value.GetStableHashCode();
+                if (value.GetStableHashCode() > 1000)
+                {
+                    UID = (Skills.SkillType)value.GetStableHashCode();
+                }
+                else
+                {
+                    throw new System.Exception("This identifier may conflict with default Valheim skills, please choose a different (or longer) identifer");
+                }
             }
         }
         public Skills.SkillType UID { get; private set; }
@@ -22,6 +29,30 @@ namespace JotunnLib.Entities
         public string Description { get; set; }
         public Sprite Icon { get; set; }
         public float IncreaseStep { get; set; }
+
+        /// <summary>
+        /// Creates a SkillConfig object for mods that previously used SkillInjector
+        /// </summary>
+        /// <param name="identifier">Unique identifier of the new skill, ex: "com.jotunnlib.testmod.testskill"</param>
+        /// <param name="uid">"id" from SkillInjector</param>
+        /// <param name="name">"name" from SkillInjector</param>
+        /// <param name="description">"description" from SkillInjector</param>
+        /// <param name="increaseStep">"increment" from SkillInjector</param>
+        /// <param name="icon">"icon" from SkillInjector</param>
+        /// <returns>New SkillConfig object that bridges SkillInjector to JotunnLib without losing user progress</returns>
+        /// <remarks>For any new skills please do not use this method!</remarks>
+        public static SkillConfig FromSkillInjector(string identifier, Skills.SkillType uid, string name, string description, float increaseStep, Sprite icon)
+        {
+            return new SkillConfig()
+            {
+                Identifier = identifier,
+                UID = uid, // Overrides hash UID
+                Name = name,
+                Description = description,
+                Icon = icon,
+                IncreaseStep = increaseStep
+            };
+        }
 
         // BaseSkill and JSON support targets v0.2.0
         //private Skills.SkillType BaseSkill { get; set; }
