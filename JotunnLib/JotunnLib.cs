c7350132c9f863bf6a7c607a927905d4f4c37d3f
Algorithman <algorithman.chris@googlemail.com>
2021-03-30 19:48:28 +0200

    Patch Initializer

--- JotunnLib/JotunnLib.cs
+++ JotunnLib/JotunnLib.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using BepInEx;
-using HarmonyLib;
 using JotunnLib.ConsoleCommands;
 using JotunnLib.Managers;
+using JotunnLib.Utils;
 
 namespace JotunnLib
 {
@@ -35,9 +35,8 @@ namespace JotunnLib
 
         private void Awake()
         {
-            // Initialize harmony patches
-            Harmony harmony = new Harmony("jotunnlib");
-            harmony.PatchAll();
+            // Initialize the patches
+            PatchInitializer.InitializePatches();
 
             // Create and initialize all managers
             RootObject = new GameObject("_JotunnLibRoot");
---
+++ JotunnLib/Utils/PatchInitializer.cs
@@ -0,0 +1,32 @@
+﻿// JotunnLib
+// a Valheim mod
+// 
+// File:    PatchInitializer.cs
+// Project: JotunnLib
+
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+namespace JotunnLib.Utils
+{
+    public abstract class PatchInitializer
+    {
+        internal abstract void Init();
+
+        public static void InitializePatches()
+        {
+            // Reflect through everything
+
+            foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var type in asm.GetTypes().Where(x=>x.BaseType==typeof(PatchInitializer)))
+                {
+                    PatchInitializer initializer = (PatchInitializer)Activator.CreateInstance(type);
+                    initializer.Init();
+                }
+            }
+        }
+    }
+}
