0d97435286072f4d8466b1b6559c1b19e569c999
Algorithman <algorithman.chris@googlemail.com>
2021-03-30 21:00:24 +0200

    refactor: Skills patches (unfinished)

--- JotunnLib/Patches/SkillsPatches.cs
+++ JotunnLib/Patches/SkillsPatches.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
-using JotunnLib.Managers;
+﻿using JotunnLib.Managers;
 using JotunnLib.Utils;
+using UnityEngine;
 
 namespace JotunnLib.Patches
 {
@@ -10,15 +10,52 @@ namespace JotunnLib.Patches
         {
             On.Skills.Awake += Skills_Awake;
             On.Skills.IsSkillValid += Skills_IsSkillValid;
+            On.Skills.CheatRaiseSkill += Skills_CheatRaiseSkill;
+            On.Skills.CheatResetSkill += Skills_CheatResetSkill;
+        }
+
+        private void Skills_CheatResetSkill(On.Skills.orig_CheatResetSkill orig, Skills self, string name)
+        {
+            foreach (var config in SkillManager.Instance.Skills)
+            {
+                if (config.Value.Name.ToLower() == name.ToLower())
+                {
+                    self.m_player.GetSkills().ResetSkill(config.Value.UID);
+                    return;
+                }
+            }
+
+            orig(self, name);
+        }
+
+        private void Skills_CheatRaiseSkill(On.Skills.orig_CheatRaiseSkill orig, Skills self, string name, float value)
+        {
+
+
+            foreach (var config in SkillManager.Instance.Skills)
+            {
+                if (config.Value.Name.ToLower() == name.ToLower())
+                {
+                    Skills.Skill skill = self.GetSkill(config.Value.UID);
+
+                    skill.m_level += value;
+                    skill.m_level = Mathf.Clamp(skill.m_level, 0f, 100f);
+                    self.m_player.Message(MessageHud.MessageType.TopLeft, "Skill increased " + config.Value.Name + ": " + (int)skill.m_level, 0, skill.m_info.m_icon);
+                    Console.instance.Print("Skill " + config.Value.Name + " = " + skill.m_level);
+                    return;
+                }
+            }
+
+            orig(self, name, value);
         }
 
         private static bool Skills_IsSkillValid(On.Skills.orig_IsSkillValid orig, Skills self, Skills.SkillType type)
         {
             foreach (var pair in SkillManager.Instance.Skills)
             {
-                if (pair.Value.m_skill == type)
+                if (pair.Value.UID == type)
                 {
-                     return true;
+                    return true;
                 }
             }
 
@@ -37,46 +74,6 @@ namespace JotunnLib.Patches
                 System.Console.WriteLine("Added extra skill: " + skill.m_skill);
             }
 
-        }
-
-        [HarmonyPatch(typeof(Skills), "CheatRaiseSkill")]
-        public static class CheatRaiseSkillPatch
-        {
-            [HarmonyPrefix]
-            public static bool Prefix(string name, float value, Skills __instance, Player ___m_player)
-            {
-                foreach (var config in SkillManager.Instance.Skills)
-                {
-                    if (config.Value.Name.ToLower() == name.ToLower())
-                    {
-                        Skills.Skill skill = Traverse.Create(__instance).Method("GetSkill", config.Value.UID).GetValue<Skills.Skill>(config.Value.UID);
-                        skill.m_level += value;
-                        skill.m_level = Mathf.Clamp(skill.m_level, 0f, 100f);
-                        ___m_player.Message(MessageHud.MessageType.TopLeft, "Skill increased " + config.Value.Name + ": " + (int)skill.m_level, 0, skill.m_info.m_icon);
-                        Console.instance.Print("Skill " + config.Value.Name + " = " + skill.m_level);
-                        return false;
-                    }
-                }
-                return true;
-            }
-        }
-
-        [HarmonyPatch(typeof(Skills), "CheatResetSkill")]
-        public static class CheatResetSkillPatch
-        {
-            [HarmonyPrefix]
-            public static bool Prefix(string name, Skills __instance, Player ___m_player)
-            {
-                foreach (var config in SkillManager.Instance.Skills)
-                {
-                    if (config.Value.Name.ToLower() == name.ToLower())
-                    {
-                        ___m_player.GetSkills().ResetSkill(config.Value.UID);
-                        return false;
-                    }
-                }
-                return true;
-            }
         }
     }
 }
