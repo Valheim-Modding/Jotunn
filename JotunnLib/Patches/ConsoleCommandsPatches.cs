2d6a6e8903771a586361c52060a5921a84c42424
Algorithman <algorithman.chris@googlemail.com>
2021-03-30 19:49:06 +0200

    feat: Console Commands patches

--- JotunnLib/Patches/ConsoleCommandsPatches.cs
+++ JotunnLib/Patches/ConsoleCommandsPatches.cs
@@ -1,75 +1,79 @@
 ﻿using System;
 using System.Text.RegularExpressions;
-using HarmonyLib;
 using JotunnLib.Managers;
 using JotunnLib.Entities;
+using JotunnLib.Utils;
+using Steamworks;
 
 namespace JotunnLib.Patches
 {
-    internal class ConsoleCommandsPatches
+    internal class ConsoleCommandsPatches : PatchInitializer
     {
-        [HarmonyPatch(typeof(Console), "InputText")]
-        public static class ConsoleInputTextPatch
+        internal override void Init()
         {
-            public static void Postfix(ref Console __instance)
+            On.Console.InputText += Console_InputText;
+        }
+
+        private void Console_InputText(On.Console.orig_InputText orig, Console self)
+        {
+            orig(self);
+
+            string text = self.m_input.text;
+            string[] parts = text.Split(' ');
+
+            if (string.IsNullOrEmpty(text) && parts.Length == 0)
             {
-                string text = __instance.m_input.text;
-                string[] parts = text.Split(' ');
-
-                if (string.IsNullOrEmpty(text) && parts.Length == 0)
-                {
-                    __instance.Print("Invalid command");
-                    return;
-                }
-
-                ConsoleCommand cmd = CommandManager.Instance.ConsoleCommands.Find(c => c.Name == parts[0]);
-
-                // If we found a command, execute it
-                if (cmd != null)
-                {
-                    /*
-                    
-                    TODO: Make arguments split take quotes into account
-
-                    string argsStr = "";
-                    
-                    if (text.Contains(' '))
-                    {
-                        argsStr = text.Substring(text.IndexOf(' '), text.Length);
-                    }
-                    
-                    string[] args = Regex.Matches(argsStr, @",(?=([^\"]*\"[^\"]*\")*[^\"]*$)")
-                        .Cast<Match>()
-                        .Select(m => m.Value)
-                        .ToArray();
-                    */
-
-                    string[] args = new string[parts.Length - 1];
-                    Array.Copy(parts, 1, args, 0, parts.Length - 1);
-                    cmd.Run(args);
-                    return;
-                }
-
-                // If a default command, don't display error
-                if (CommandManager.DefaultConsoleCommands.Contains(parts[0]))
-                {
-                    return;
-                }
-
-                // If a cheat command, check if cheats enabled
-                if (CommandManager.DefaultCheatConsoleCommands.Contains(parts[0]))
-                {
-                    if (!Console.instance.IsCheatsEnabled())
-                    {
-                        __instance.Print("Cannot use this command without cheats enabled. Use 'imacheater' to enable cheats");
-                    }
-
-                    return;
-                }
-
-                // Display error otherwise
-                __instance.Print("Invalid command");
+                self.Print("Invalid command");
+                return;
             }
+
+            ConsoleCommand cmd = CommandManager.Instance.ConsoleCommands.Find(c => c.Name == parts[0]);
+
+            // If we found a command, execute it
+            if (cmd != null)
+            {
+                /*
+
+                TODO: Make arguments split take quotes into account
+
+                string argsStr = "";
+
+                if (text.Contains(' '))
+                {
+                    argsStr = text.Substring(text.IndexOf(' '), text.Length);
+                }
+
+                string[] args = Regex.Matches(argsStr, @",(?=([^\"]*\"[^\"]*\")*[^\"]*$)")
+                    .Cast<Match>()
+                    .Select(m => m.Value)
+                    .ToArray();
+                */
+
+                string[] args = new string[parts.Length - 1];
+                Array.Copy(parts, 1, args, 0, parts.Length - 1);
+                cmd.Run(args);
+                return;
+            }
+
+            // If a default command, don't display error
+            if (CommandManager.DefaultConsoleCommands.Contains(parts[0]))
+            {
+                return;
+            }
+
+            // If a cheat command, check if cheats enabled
+            if (CommandManager.DefaultCheatConsoleCommands.Contains(parts[0]))
+            {
+                if (!Console.instance.IsCheatsEnabled())
+                {
+                    self.Print("Cannot use this command without cheats enabled. Use 'imacheater' to enable cheats");
+                }
+
+                return;
+            }
+
+            // Display error otherwise
+            self.Print("Invalid command");
         }
     }
 }
