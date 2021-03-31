21e7abdf72648762df3ba7177924119f6068e550
Algorithman <algorithman.chris@googlemail.com>
2021-03-31 02:20:49 +0200

    refactor: Added priority attribute for patch initializers

---
+++ JotunnLib/Utils/PatchPriorityAttribute.cs
@@ -0,0 +1,20 @@
+﻿// JotunnLib
+// a Valheim mod
+// 
+// File:    PatchPriorityAttribute.cs
+// Project: JotunnLib
+
+using System;
+
+namespace JotunnLib.Utils
+{
+    public class PatchPriorityAttribute : Attribute
+    {
+        public int Priority { get; set; }
+
+        public PatchPriorityAttribute(int priority)
+        {
+            Priority = priority;
+        }
+    }
+}
