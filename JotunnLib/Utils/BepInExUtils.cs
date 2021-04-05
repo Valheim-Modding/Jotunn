5fd9c9a66f8bc1d16ff56e3338126130374ac562
Algorithman <algorithman.chris@googlemail.com>
2021-04-05 17:39:02 +0200

    extend GetDependentPlugins to add JotunnLib itself if arg is true

--- JotunnLib/Utils/BepInExUtils.cs
+++ JotunnLib/Utils/BepInExUtils.cs
@@ -10,14 +10,20 @@ namespace JotunnLib.Utils
         ///     Get a dictionary of loaded plugins which depend on JotunnLib
         /// </summary>
         /// <returns></returns>
-        internal static Dictionary<string, BaseUnityPlugin> GetDependentPlugins()
+        internal static Dictionary<string, BaseUnityPlugin> GetDependentPlugins(bool includeJotunnLib = false)
         {
             var result = new Dictionary<string, BaseUnityPlugin>();
 
-            var plugins = BepInEx.Bootstrap.Chainloader.PluginInfos.Select(x=>x.Value.Instance).ToArray();
+            var plugins = BepInEx.Bootstrap.Chainloader.PluginInfos.Select(x => x.Value.Instance).ToArray();
 
             foreach (var plugin in plugins)
             {
+                if (includeJotunnLib && plugin.Info.Metadata.GUID == Main.ModGuid)
+                {
+                    result.Add(plugin.Info.Metadata.GUID, plugin);
+                    continue;
+                }
+
                 foreach (var dependencyAttribute in plugin.GetType().GetCustomAttributes(typeof(BepInDependency), false).Cast<BepInDependency>())
                 {
                     if (dependencyAttribute.DependencyGUID == Main.ModGuid)
