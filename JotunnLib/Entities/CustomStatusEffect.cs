b899754de50fa94978eae220e973fd08bac1de2d
Jules <github@sirskunkalot.de>
2021-04-08 16:29:53 +0200

    chore: add xmldoc to all entities

--- JotunnLib/Entities/CustomStatusEffect.cs
+++ JotunnLib/Entities/CustomStatusEffect.cs
@@ -2,13 +2,41 @@
 {
     public class CustomStatusEffect
     {
-        public StatusEffect StatusEffect;
-        public bool FixReference;
+        /// <summary>
+        ///     The <see cref="StatusEffect"/> for this custom status effect.
+        /// </summary>
+        public StatusEffect StatusEffect { get; set; }
 
+        /// <summary>
+        ///     Indicator if references from <see cref="Mock"/>s will be replaced at runtime.
+        /// </summary>
+        public bool FixReference { get; set; }
+
+        /// <summary>
+        ///     Custom status effect from a <see cref="global::StatusEffect"/>.<br />
+        ///     Can fix references for <see cref="Mock"/>s.
+        /// </summary>
+        /// <param name="statusEffect">A preloaded <see cref="global::StatusEffect"/></param>
+        /// <param name="fixReference">If true references for <see cref="Mock"/> objects get resolved at runtime by Jötunn.</param>
         public CustomStatusEffect(StatusEffect statusEffect, bool fixReference)
         {
             StatusEffect = statusEffect;
             FixReference = fixReference;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj.GetHashCode() == GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            return StatusEffect.name.GetStableHashCode();
+        }
+
+        public override string ToString()
+        {
+            return StatusEffect.name;
         }
     }
 }
