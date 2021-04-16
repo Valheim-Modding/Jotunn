8934516f030bb277b19b681de1c249da4ce728c2
Jules <github@sirskunkalot.de>
2021-04-16 02:09:20 +0200

    refactor: remove register/load from InputManager, closes #107, closes #108

--- JotunnLib/Managers/InputManager.cs
+++ JotunnLib/Managers/InputManager.cs
@@ -19,99 +19,25 @@ namespace JotunnLib.Managers
         }
         internal static Dictionary<string, ButtonConfig> Buttons = new Dictionary<string, ButtonConfig>();
 
-        public EventHandler InputRegister;
-        private bool inputsRegistered = false;
-
 
         public void Init()
         {
-            On.ZInput.Initialize += ZInput_Initialize;
             On.ZInput.Reset += ZInput_Reset;
             On.ZInput.GetButtonDown += ZInput_GetButtonDown;
             On.ZInput.GetButtonUp += ZInput_GetButtonUp;
         }
 
-        private bool ZInput_GetButtonUp(On.ZInput.orig_GetButtonUp orig, string name)
+        public void AddButton(string modguid, ButtonConfig button)
         {
-            var result = orig(name);
-            if (!result)
+            if (Buttons.ContainsKey(button.Name + "!" + modguid))
             {
-                foreach (var buttonDef in ZInput.instance.m_buttons.Where(x => x.Key.StartsWith(name + "!")))
-                {
-                    if (Time.inFixedTimeStep ? buttonDef.Value.m_upFixed : buttonDef.Value.m_up)
-                    {
-                        return true;
-                    }
-                }
-            }
-
-            return result;
-        }
-
-        private bool ZInput_GetButtonDown(On.ZInput.orig_GetButtonDown orig, string name)
-        {
-            var result = orig(name);
-            if (!result)
-            {
-                foreach (var def in ZInput.instance.m_buttons.Where(x => x.Key.StartsWith(name + "!")))
-                {
-                    if (Time.inFixedTimeStep ? def.Value.m_downFixed : def.Value.m_down)
-                    {
-                        return true;
-                    }
-                }
-            }
-
-            return result;
-        }
-
-        internal void Load(ZInput zinput)
-        {
-            Logger.LogInfo("---- Registering custom inputs ----");
-
-            if (zinput == null)
-            {
-                Logger.LogError("\t-> ZInput does not exist yet, delaying...");
+                Logger.LogError("Cannot have duplicate button: " + button.Name);
                 return;
             }
 
-            foreach (var pair in Buttons)
-            {
-                var btn = pair.Value;
+            button.Name += "!" + modguid;
 
-                if (btn.Axis != null && btn.Axis.Length > 0)
-                {
-                    zinput.AddButton(btn.Name, btn.Axis, btn.Inverted, btn.RepeatDelay, btn.RepeatInterval);
-                }
-                else
-                {
-                    zinput.AddButton(btn.Name, btn.Key, btn.RepeatDelay, btn.RepeatInterval);
-                }
-
-                Logger.LogInfo("Registered input: " + pair.Key);
-            }
-        }
-
-        internal void Register()
-        {
-            if (inputsRegistered)
-            {
-                return;
-            }
-
-            InputRegister?.Invoke(null, EventArgs.Empty);
-            inputsRegistered = true;
-        }
-
-        public void AddButton(string name, ButtonConfig button)
-        {
-            if (Buttons.ContainsKey(name))
-            {
-                Logger.LogError("Cannot have duplicate button: " + name);
-                return;
-            }
-
-            Buttons.Add(name, button);
+            Buttons.Add(button.Name, button);
         }
 
         public void AddButton(
@@ -161,20 +87,67 @@ namespace JotunnLib.Managers
             });
         }
 
+        private bool ZInput_GetButtonUp(On.ZInput.orig_GetButtonUp orig, string name)
+        {
+            var result = orig(name);
+            if (!result)
+            {
+                foreach (var buttonDef in ZInput.instance.m_buttons.Where(x => x.Key.StartsWith(name + "!")))
+                {
+                    if (Time.inFixedTimeStep ? buttonDef.Value.m_upFixed : buttonDef.Value.m_up)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private bool ZInput_GetButtonDown(On.ZInput.orig_GetButtonDown orig, string name)
+        {
+            var result = orig(name);
+            if (!result)
+            {
+                foreach (var def in ZInput.instance.m_buttons.Where(x => x.Key.StartsWith(name + "!")))
+                {
+                    if (Time.inFixedTimeStep ? def.Value.m_downFixed : def.Value.m_down)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return result;
+        }
 
         private void ZInput_Reset(On.ZInput.orig_Reset orig, ZInput self)
         {
             orig(self);
-            Logger.LogDebug("----> ZInput Reset");
-            Load(self);
-        }
 
-        private void ZInput_Initialize(On.ZInput.orig_Initialize orig)
-        {
-            Logger.LogDebug("----> ZInput Initialize");
-            Register();
-            orig();
-        }
+            Logger.LogInfo("---- Registering custom inputs ----");
 
+            if (self == null)
+            {
+                Logger.LogError("\t-> ZInput does not exist yet, delaying...");
+                return;
+            }
+
+            foreach (var pair in Buttons)
+            {
+                var btn = pair.Value;
+
+                if (btn.Axis != null && btn.Axis.Length > 0)
+                {
+                    self.AddButton(btn.Name, btn.Axis, btn.Inverted, btn.RepeatDelay, btn.RepeatInterval);
+                }
+                else
+                {
+                    self.AddButton(btn.Name, btn.Key, btn.RepeatDelay, btn.RepeatInterval);
+                }
+
+                Logger.LogInfo("Registered input: " + pair.Key);
+            }
+        }
     }
 }
