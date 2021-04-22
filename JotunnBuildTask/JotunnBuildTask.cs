f36450814df50c735a884837c96f7dcae3bb43d7
Jules <github@sirskunkalot.de>
2021-04-22 02:06:46 +0200

    refactor: build task

---
+++ JotunnBuildTask/JotunnBuildTask.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using Mono.Cecil;
+using MonoMod;
+using MonoMod.RuntimeDetour.HookGen;
+using Microsoft.Build.Framework;
+
+namespace JotunnBuildTask
+{
+    public class JotunnBuildTask : Microsoft.Build.Utilities.Task
+    {
+        [Required]
+        public string ValheimPath { get; set; }
+
+        internal const string ValheimServerData = "valheim_server_Data";
+        internal const string ValheimData= "valheim_Data";
+        internal const string Managed = "Managed";
+        internal const string UnstrippedCorlib = "unstripped_corlib";
+        internal const string PublicizedAssemblies = "publicized_assemblies";
+        internal const string Bepinex = "BepInEx";
+        internal const string Plugins = "plugins";
+        internal const string Mmhook = "MMHOOK";
+        internal const string Publicized = "publicized";
+
+        /// <summary>
+        ///     Hash file and compare with old hash.
+        /// </summary>
+        /// <param name="assembly">Valheim assembly</param>
+        /// <param name="managedPath">Path to managed assemblies</param>
+        private bool HashAndCompare(string assembly, out string hash)
+        {
+
+            Log.LogMessage(MessageImportance.High, $"Processing {assembly}");
+
+            hash = MD5HashFile(assembly);
+
+            string hashFilePath = assembly + ".hash";
+            if (File.Exists(hashFilePath))
+            {
+                // read hash and compare
+                var oldHash = File.ReadAllText(hashFilePath);
+                if (hash == oldHash)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        ///     Hash file (MD5).
+        /// </summary>
+        /// <param name="filename">filename</param>
+        /// <returns>MD5 hash</returns>
+        internal string MD5HashFile(string filename)
+        {
+            var hash = MD5.Create().ComputeHash(File.ReadAllBytes(filename));
+            return BitConverter.ToString(hash).Replace("-", "");
+        }
+
+        public override bool Execute()
+        {
+
+            Log.LogMessage(MessageImportance.High, "Current folder:  " + Environment.CurrentDirectory);
+            Log.LogMessage(MessageImportance.High, "DLL Folder: " + Assembly.GetExecutingAssembly().CodeBase);
+
+            try
+            {
+                // Get managed folder of valheim or valheim_dedicated
+                string managedFolder = string.Empty;
+                if (Directory.Exists(Path.Combine(ValheimPath, ValheimData, Managed)))
+                {
+                    managedFolder = Path.Combine(ValheimPath, ValheimData, Managed);
+                }
+                if (Directory.Exists(Path.Combine(ValheimPath, ValheimServerData, Managed)))
+                {
+                    managedFolder = Path.Combine(ValheimPath, ValheimServerData, Managed);
+                }
+                if (string.IsNullOrEmpty(managedFolder))
+                {
+                    throw new Exception($"{ValheimPath} does not include {ValheimData} or {ValheimServerData}");
+                }
+
+                // Get publicized folder
+                string publicizedFolder = Path.Combine(managedFolder, PublicizedAssemblies);
+                if (!Directory.Exists(publicizedFolder))
+                {
+                    Directory.CreateDirectory(publicizedFolder);
+                }
+
+                // Get MMHOOK folder
+                string mmhookFolder = Path.Combine(ValheimPath, Bepinex, Plugins, Mmhook);
+                if (!Directory.Exists(mmhookFolder))
+                {
+                    Directory.CreateDirectory(mmhookFolder);
+                }
+
+                // Loop assemblies and check if the hash has changed
+                foreach (var assembly in Directory.GetFiles(managedFolder, "assembly_*.dll"))
+                {
+                    if (!HashAndCompare(assembly, out var hash))
+                    {
+                        try
+                        {
+                            // Try to publicize
+                            if (!AssemblyPublicizer.PublicizeDll(assembly, publicizedFolder, ValheimPath, Log))
+                            {
+                                return false;
+                            }
+
+                            // Try to generate MMHook
+                            if (!MMHookGenerator.GenerateMMHook(assembly, mmhookFolder, hash, ValheimPath, Log))
+                            {
+                                return false;
+                            }
+
+                            // Write current hash to file if all previous actions succeeded
+                            File.WriteAllText(assembly + ".hash", hash);
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.LogError($"Error occured on {assembly}");
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.LogError(e.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ JotunnBuildTask/MMHookGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Mono.Cecil;
+using MonoMod;
+using MonoMod.RuntimeDetour.HookGen;
+using TypeAttributes = Mono.Cecil.TypeAttributes;
+
+namespace JotunnBuildTask
+{
+    public static class MMHookGenerator
+    {
+
+        /// <summary>
+        ///     Call Monomod hookgen.
+        /// </summary>
+        /// <param name="input">Input assembly</param>
+        /// <param name="mmhookFolder">MMHOOK output folder</param>
+        /// <returns></returns>
+        public static bool GenerateMMHook(string input, string mmhookFolder, string md5, string ValheimPath, TaskLoggingHelper Log)
+        {
+            string output = Path.Combine(mmhookFolder, $"MMHOOK_{Path.GetFileName(input)}");
+
+            MonoModder modder = new MonoModder();
+            modder.InputPath = input;
+            modder.OutputPath = output;
+            modder.ReadingMode = ReadingMode.Deferred;
+
+            ((BaseAssemblyResolver)modder.AssemblyResolver)?.AddSearchDirectory(Path.Combine(Environment.CurrentDirectory, "bin", "Debug"));
+            ((BaseAssemblyResolver)modder.AssemblyResolver)?.AddSearchDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase.Replace("file:///home", "/home").Replace("file:///", "")));
+
+            if (Directory.Exists(Path.Combine(ValheimPath, JotunnBuildTask.ValheimData, JotunnBuildTask.Managed)))
+            {
+                ((BaseAssemblyResolver)modder.AssemblyResolver)?.AddSearchDirectory(Path.Combine(ValheimPath, JotunnBuildTask.ValheimData, JotunnBuildTask.Managed));
+            }
+
+            if (Directory.Exists(Path.Combine(ValheimPath, JotunnBuildTask.ValheimServerData, JotunnBuildTask.Managed)))
+            {
+                ((BaseAssemblyResolver)modder.AssemblyResolver)?.AddSearchDirectory(Path.Combine(ValheimPath, JotunnBuildTask.ValheimServerData, JotunnBuildTask.Managed));
+            }
+            ((BaseAssemblyResolver)modder.AssemblyResolver)?.AddSearchDirectory(Path.Combine(ValheimPath, JotunnBuildTask.UnstrippedCorlib));
+
+            /*
+            foreach (var dir in ((BaseAssemblyResolver) modder.AssemblyResolver)?.GetSearchDirectories())
+            {
+                Log.LogMessage(MessageImportance.High,$"Searching in {dir}");
+            }
+            */
+
+            modder.Read();
+
+            modder.MapDependencies();
+
+            if (File.Exists(output))
+            {
+                Log.LogMessage(MessageImportance.High, $"Clearing {output}");
+                File.Delete(output);
+            }
+
+            HookGenerator hookGenerator = new HookGenerator(modder, Path.GetFileName(output));
+
+            hookGenerator.HookPrivate = true;
+
+            using (ModuleDefinition mOut = hookGenerator.OutputModule)
+            {
+                hookGenerator.Generate();
+                mOut.Types.Add(new TypeDefinition("BepHookGen", "hash" + md5, TypeAttributes.AutoClass));
+                mOut.Write(output);
+            }
+
+            Log.LogMessage(MessageImportance.High, $"Finished writing {output}");
+
+            return true;
+        }
+
+    }
+}
