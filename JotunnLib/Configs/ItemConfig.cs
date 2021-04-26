2b47d887558ae39786325dc53f7fdff66680708f
Jules <github@sirskunkalot.de>
2021-04-26 21:46:46 +0200

    docs: add xmldocs for item/recipe configs

--- JotunnLib/Configs/ItemConfig.cs
+++ JotunnLib/Configs/ItemConfig.cs
@@ -4,17 +4,57 @@ using Jotunn.Entities;
 
 namespace Jotunn.Configs
 {
+    /// <summary>
+    ///     Configuration class for adding custom items. Automatically creates a recipe for this item.
+    /// </summary>
     public class ItemConfig
     {
+        /// <summary>
+        ///     The unique name for your item.
+        /// </summary>
         public string Name { get; set; } = null;
-        public string Item { get; set; }
+
+        /// <summary>
+        ///     The name of the item prefab that this recipe should create. Is automatically set in <see cref="CustomItem"/>.
+        /// </summary>
+        internal string Item { get; set; }
+
+        /// <summary>
+        ///     The amount of <see cref="Item"/> that will be created from the recipe.
+        /// </summary>
         public int Amount { get; set; } = 1;
+
+        /// <summary>
+        ///     Whether this item is craftable or not.
+        /// </summary>
         public bool Enabled { get; set; } = true;
+
+        /// <summary>
+        ///     The name of the crafting station prefab where this recipe can be crafted.<br/>
+        ///     Can be set to <c>null</c> to have the recipe be craftable without a crafting station.
+        /// </summary>
         public string CraftingStation { get; set; } = null;
+
+        /// <summary>
+        ///     The name of the crafting station prefab where this item can be repaired.<br/>
+        ///     Can be set to <c>null</c> to have the recipe be repairable without a crafting station.
+        /// </summary>
         public string RepairStation { get; set; } = null;
+
+        /// <summary>
+        ///     The minimum required level for the crafting station. Defaults to <c>0</c>.
+        /// </summary>
         public int MinStationLevel { get; set; }
+
+        /// <summary>
+        ///     Array of <see cref="RequirementConfig"/>s for all crafting materials it takes to craft the recipe.
+        /// </summary>
         public RequirementConfig[] Requirements { get; set; } = new RequirementConfig[0];
 
+        /// <summary>
+        ///     Converts the RequirementConfigs to Valheim style Piece.Requirements
+        /// </summary>
+        /// <returns>The Valheim Piece.Requirement array</returns>
         public Piece.Requirement[] GetRequirements()
         {
             Piece.Requirement[] reqs = new Piece.Requirement[Requirements.Length];
@@ -27,6 +67,10 @@ namespace Jotunn.Configs
             return reqs;
         }
 
+        /// <summary>
+        ///     Converts the ItemConfig to a Valheim style Recipe.
+        /// </summary>
+        /// <returns>The Valheim recipe</returns>
         public Recipe GetRecipe()
         {
             if (Item == null)
--- JotunnLib/Configs/RequirementConfig.cs
+++ JotunnLib/Configs/RequirementConfig.cs
@@ -2,13 +2,35 @@
 
 namespace Jotunn.Configs
 {
+    /// <summary>
+    ///     Configuration class for requirements needed to craft items or build pieces.
+    /// </summary>
     public class RequirementConfig
     {
+        /// <summary>
+        ///     Name of the item prefab of this requirement.
+        /// </summary>
         public string Item { get; set; }
+
+        /// <summary>
+        ///     Amount that is needed to fulfill the requirement.
+        /// </summary>
         public int Amount { get; set; }
+
+        /// <summary>
+        ///     How much more of this requirement is needed per item level.
+        /// </summary>
         public int AmountPerLevel { get; set; }
+
+        /// <summary>
+        ///     Determines if the used requirement will be rewarded again after dismanteling a piece.
+        /// </summary>
         public bool Recover { get; set; }
 
+        /// <summary>
+        ///     Creates a Valheim Piece.Requirement from this config. 
+        /// </summary>
+        /// <returns></returns>
         public Piece.Requirement GetRequirement()
         {
             return new Piece.Requirement()
