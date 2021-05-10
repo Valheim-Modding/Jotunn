345a947cff99565ef9af7db7b1425867f1cf6c50
Jules <github@sirskunkalot.de>
2021-05-10 13:36:39 +0200

    docs: fix piece list generation and gather new vanilla lists

---
+++ JotunnDoc/Properties/IgnoreAccessModifiers.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Permissions;
+
+// SecurityPermision set to minimum
+// for skipping access modifiers check from the mono JIT
+// The same attribute are added to the assembly when ticking
+// Unsafe Code in the Project settings
+// This is done here to allow an explanation of the trick and
+// not in an outside source you could potentially miss.
+
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
