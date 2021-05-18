fb8feda7e1e87e3145241502a18db7b30c7c8345
Jules <github@sirskunkalot.de>
2021-05-18 21:39:35 +0200

    feat: add more properties and Apply() method

--- JotunnLib/Configs/ItemConfig.cs
+++ JotunnLib/Configs/ItemConfig.cs
@@ -12,22 +12,27 @@ namespace Jotunn.Configs
     public class ItemConfig
     {
         /// <summary>
-        ///     The unique name for your item.
+        ///     The unique name for your item. May be tokenized.
         /// </summary>
-        public string Name { get; set; } = null;
+        public string Name { get; set; } = string.Empty;
 
         /// <summary>
-        ///     The name of the item prefab that this recipe should create. Is automatically set in <see cref="CustomItem"/>.
+        ///     The description of your item. May be tokenized.
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        ///     The name of the item prefab. Is automatically set in <see cref="CustomItem"/>.
         /// </summary>
         internal string Item { get; set; }
 
         /// <summary>
-        ///     The amount of <see cref="Item"/> that will be created from the recipe.
+        ///     The amount of <see cref="Item"/> that will be created when crafting this item. Defaults to <c>1</c>.
         /// </summary>
         public int Amount { get; set; } = 1;
 
         /// <summary>
-        ///     Whether this item is craftable or not.
+        ///     Whether this item is craftable or not. Defaults to <c>true</c>.
         /// </summary>
         public bool Enabled { get; set; } = true;
 
@@ -39,19 +44,67 @@ namespace Jotunn.Configs
 
         /// <summary>
         ///     The name of the crafting station prefab where this item can be repaired.<br/>
-        ///     Can be set to <c>null</c> to have the recipe be repairable without a crafting station.
+        ///     Can be set to <c>null</c> to have the item be repairable without a crafting station.
         /// </summary>
         public string RepairStation { get; set; } = null;
 
         /// <summary>
         ///     The minimum required level for the crafting station. Defaults to <c>0</c>.
         /// </summary>
-        public int MinStationLevel { get; set; }
+        public int MinStationLevel { get; set; } = 0;
+
+        /// <summary>
+        ///     Icons for this item. If more than one icon is added, this item automatically has variants.
+        /// </summary>
+        public Sprite[] Icons { get; set; } = null;
 
         /// <summary>
         ///     Array of <see cref="RequirementConfig"/>s for all crafting materials it takes to craft the recipe.
         /// </summary>
         public RequirementConfig[] Requirements { get; set; } = new RequirementConfig[0];
+
+        public void Apply(GameObject prefab)
+        {
+            var itemDrop = prefab.GetComponent<ItemDrop>();
+            if (itemDrop == null)
+            {
+                Logger.LogError($"GameObject has no ItemDrop attached");
+                return;
+            }
+
+            // Set the items name from the prefab
+            Item = prefab.name;
+
+            var shared = itemDrop.m_itemData.m_shared;
+            if (shared == null)
+            {
+                Logger.LogError($"ItemDrop has no SharedData component");
+                return;
+            }
+
+            // Set the name and description if provided
+            if (!string.IsNullOrEmpty(Name))
+            {
+                shared.m_name = Name;
+            }
+            if (!string.IsNullOrEmpty(Description))
+            {
+                shared.m_description = Description;
+            }
+
+            // If there is still no m_name, add a token from the prefabs name
+            if (string.IsNullOrEmpty(shared.m_name))
+            {
+                shared.m_name = $"${prefab.name}";
+            }
+
+            // Set icons if provided
+            if (Icons != null && Icons.Length > 0)
+            {
+                itemDrop.m_itemData.m_shared.m_variants = Icons.Length;
+                itemDrop.m_itemData.m_shared.m_icons = Icons;
+            }
+        }
 
         /// <summary>
         ///     Converts the RequirementConfigs to Valheim style Piece.Requirements
