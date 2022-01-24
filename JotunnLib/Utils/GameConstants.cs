be9a4485b8f8944d841ad7af5a523bbc6a18a074
Jules <github@sirskunkalot.de>
2022-01-25 00:21:18 +0100

    feat: add game string constants

---
+++ JotunnLib/Utils/GameConstants.cs
@@ -0,0 +1,54 @@
+﻿#pragma warning disable CS1591 // Missing XML comment
+namespace Jotunn.Utils
+{
+    /// <summary>
+    ///     Constants of hardcoded strings used in-game
+    /// </summary>
+    public static class GameConstants
+    {
+        /// <summary>
+        ///     GlobalKey strings
+        /// </summary>
+        public static class GlobalKey
+        {
+            public static readonly string None = "";
+            public static readonly string KilledBonemass = "defeated_bonemass";
+            public static readonly string KilledElder = "defeated_gdking";
+            public static readonly string KilledYagluth = "defeated_goblinking";
+            public static readonly string KilledModer = "defeated_dragon";
+            public static readonly string KilledEikthyr = "defeated_eikthyr";
+            public static readonly string KilledTroll = "KilledTroll";
+            public static readonly string KilledSurtling = "killed_surtling";
+        }
+
+        /// <summary>
+        ///     Weather strings
+        /// </summary>
+        public static class Weather
+        {
+            public static readonly string ClearSkies = "Clear";
+            public static readonly string MeadowsClearSkies = "Heath_clear";
+            public static readonly string LightRain = "LightRain";
+            public static readonly string Rain = "Rain";
+            public static readonly string ThunderStorm = "ThunderStorm";
+            public static readonly string ClearThunderStorm = "nofogts";
+            public static readonly string SwampRain = "SwampRain";
+            public static readonly string MistlandsDark = "Darklands_dark";
+            public static readonly string AshlandsAshrain = "Ashrain";
+            public static readonly string MountainSnow = "Snow";
+            public static readonly string MountainBlizzard = "SnowStorm";
+            public static readonly string BlackForestFog = "DeepForest Mist";
+            public static readonly string Fog = "Misty";
+            public static readonly string DeepNorthSnow = "Twilight_Snow";
+            public static readonly string DeepNorthSnowStorm = "Twilight_SnowStorm";
+            public static readonly string DeepNorthClear = "Twilight_Clear";
+            public static readonly string EikthyrsThunderstorm = "Eikthyr";
+            public static readonly string EldersHaze = "GDKing";
+            public static readonly string BonemassDownpour = "Bonemass";
+            public static readonly string ModersVortex = "Moder";
+            public static readonly string YagluthsMagicBlizzard = "GoblinKing";
+            public static readonly string Crypt = "Crypt";
+            public static readonly string SunkenCrypt = "SunkenCrypt";
+        }
+    }
+}
