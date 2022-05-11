84f9c9c1dc89072a95833cc1c71ff8c2355f21c4
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2022-05-11 15:13:38 +0200

    feat: Mod Query also ObjectDB

--- JotunnLib/Utils/ModQuery.cs
+++ JotunnLib/Utils/ModQuery.cs
@@ -13,6 +13,7 @@ namespace Jotunn.Utils
     public class ModQuery
     {
         private static readonly Dictionary<string, Dictionary<int, ModPrefab>> Prefabs = new Dictionary<string, Dictionary<int, ModPrefab>>();
+        private static readonly Dictionary<string, List<Recipe>> Recipes = new Dictionary<string, List<Recipe>>();
 
         internal static void Init()
         {
@@ -90,32 +91,59 @@ namespace Jotunn.Utils
         private static void FejdStartup_Awake_Postfix()
         {
             var zNetAwake = AccessTools.Method(typeof(ZNetScene), nameof(ZNetScene.Awake));
-            var prefixes = Harmony.GetPatchInfo(zNetAwake).Prefixes;
-            var postfixes = Harmony.GetPatchInfo(zNetAwake).Postfixes;
-            var finalizers = Harmony.GetPatchInfo(zNetAwake).Finalizers;
+            PatchZNetViewPatches(Harmony.GetPatchInfo(zNetAwake).Prefixes);
+            PatchZNetViewPatches(Harmony.GetPatchInfo(zNetAwake).Postfixes);
+            PatchZNetViewPatches(Harmony.GetPatchInfo(zNetAwake).Finalizers);
 
-            CatchAllAddedPrefabs(prefixes);
-            CatchAllAddedPrefabs(postfixes);
-            CatchAllAddedPrefabs(finalizers);
+            var objectDBAwake = AccessTools.Method(typeof(ObjectDB), nameof(ObjectDB.Awake));
+            PatchObjectDbPatches(Harmony.GetPatchInfo(objectDBAwake).Prefixes);
+            PatchObjectDbPatches(Harmony.GetPatchInfo(objectDBAwake).Postfixes);
+            PatchObjectDbPatches(Harmony.GetPatchInfo(objectDBAwake).Finalizers);
+
+            var objectDBCopyOtherDB = AccessTools.Method(typeof(ObjectDB), nameof(ObjectDB.CopyOtherDB));
+            PatchObjectDbPatches(Harmony.GetPatchInfo(objectDBCopyOtherDB).Prefixes);
+            PatchObjectDbPatches(Harmony.GetPatchInfo(objectDBCopyOtherDB).Postfixes);
+            PatchObjectDbPatches(Harmony.GetPatchInfo(objectDBCopyOtherDB).Finalizers);
+
+            var objectDBUpdateItemHashes = AccessTools.Method(typeof(ObjectDB), nameof(ObjectDB.UpdateItemHashes));
+            PatchObjectDbPatches(Harmony.GetPatchInfo(objectDBUpdateItemHashes).Prefixes);
+            PatchObjectDbPatches(Harmony.GetPatchInfo(objectDBUpdateItemHashes).Postfixes);
+            PatchObjectDbPatches(Harmony.GetPatchInfo(objectDBUpdateItemHashes).Finalizers);
         }
 
-        private static void CatchAllAddedPrefabs(ICollection<Patch> patches)
+        private static void PatchZNetViewPatches(ICollection<Patch> patches)
         {
             foreach (var patch in patches)
             {
-                var pre = AccessTools.Method(typeof(ModQuery), nameof(Pre));
-                var post = AccessTools.Method(typeof(ModQuery), nameof(Post));
+                var pre = AccessTools.Method(typeof(ModQuery), nameof(BeforeZNetPatch));
+                var post = AccessTools.Method(typeof(ModQuery), nameof(AfterZNetPatch));
                 Main.Harmony.Patch(patch.PatchMethod, new HarmonyMethod(pre), new HarmonyMethod(post));
             }
         }
 
-        private static void Pre(object[] __args, ref PrefabInfo __state)
+        private static void PatchObjectDbPatches(ICollection<Patch> patches)
         {
-            ZNetScene zNetScene = GetZNetScene(__args);
-            __state = new PrefabInfo(zNetScene.m_namedPrefabs, zNetScene.m_prefabs);
+            foreach (var patch in patches)
+            {
+                var pre = AccessTools.Method(typeof(ModQuery), nameof(BeforeObjectDBPatch));
+                var post = AccessTools.Method(typeof(ModQuery), nameof(AfterObjectDBPatch));
+                Main.Harmony.Patch(patch.PatchMethod, new HarmonyMethod(pre), new HarmonyMethod(post));
+            }
         }
 
-        private static void Post(object[] __args, ref PrefabInfo __state)
+        private static void BeforeZNetPatch(object[] __args, ref ZNetSceneState __state)
+        {
+            ZNetScene zNetScene = GetZNetScene(__args);
+            __state = new ZNetSceneState(zNetScene);
+        }
+
+        private static void BeforeObjectDBPatch(object[] __args, ref ObjectDBState __state)
+        {
+            ObjectDB objectDB = GetObjectDB(__args);
+            __state = new ObjectDBState(objectDB);
+        }
+
+        private static void AfterZNetPatch(object[] __args, ref ZNetSceneState __state)
         {
             ZNetScene zNetScene = GetZNetScene(__args);
             var plugin = BepInExUtils.GetPluginInfoFromAssembly(ReflectionHelper.GetCallingAssembly());
@@ -140,6 +168,40 @@ namespace Jotunn.Utils
             }
         }
 
+        private static void AfterObjectDBPatch(object[] __args, ref ObjectDBState __state)
+        {
+            ObjectDB objectDB = GetObjectDB(__args);
+            var plugin = BepInExUtils.GetPluginInfoFromAssembly(ReflectionHelper.GetCallingAssembly());
+
+            if (plugin == null)
+            {
+                Logger.LogWarning($"Assembly {ReflectionHelper.GetCallingAssembly().FullName} is not a BepInEx plugin");
+                return;
+            }
+
+            var itemsAdded = objectDB.m_items.Except(__state.items);
+            var itemByHashAdded = objectDB.m_itemByHash.Except(__state.itemByHash);
+            var recipesAdded = objectDB.m_recipes.Except(__state.recipes);
+
+            AddPrefabs(itemsAdded, plugin.Metadata);
+            AddPrefabs(itemByHashAdded.Select(i => i.Value), plugin.Metadata);
+            AddRecipes(recipesAdded, plugin.Metadata);
+
+            Logger.LogInfo($"{plugin.Metadata.GUID} added {Prefabs[plugin.Metadata.GUID].Count} prefabs");
+
+            foreach (var prefab in Prefabs[plugin.Metadata.GUID])
+            {
+                Logger.LogInfo($"\t{prefab.Value.Prefab.name}");
+            }
+
+            Logger.LogInfo($"{plugin.Metadata.GUID} added {Recipes[plugin.Metadata.GUID].Count} recipes");
+
+            foreach (var recipe in Recipes[plugin.Metadata.GUID])
+            {
+                Logger.LogInfo($"\t{recipe.m_item.name}");
+            }
+        }
+
         private static void AddPrefabs(IEnumerable<GameObject> prefabs, BepInPlugin plugin)
         {
             if (!Prefabs.ContainsKey(plugin.GUID))
@@ -154,6 +216,23 @@ namespace Jotunn.Utils
                 if (!Prefabs[plugin.GUID].ContainsKey(hash))
                 {
                     Prefabs[plugin.GUID].Add(hash, new ModPrefab(prefab, plugin));
+                }
+            }
+        }
+
+
+        private static void AddRecipes(IEnumerable<Recipe> recipes, BepInPlugin plugin)
+        {
+            if (!Recipes.ContainsKey(plugin.GUID))
+            {
+                Recipes.Add(plugin.GUID, new List<Recipe>());
+            }
+
+            foreach (var recipe in recipes)
+            {
+                if (!Recipes[plugin.GUID].Contains(recipe))
+                {
+                    Recipes[plugin.GUID].Add(recipe);
                 }
             }
         }
@@ -176,15 +255,42 @@ namespace Jotunn.Utils
             return ZNetScene.instance;
         }
 
-        private struct PrefabInfo
+        private static ObjectDB GetObjectDB(object[] __args)
+        {
+            foreach (var arg in __args)
+            {
+                if (arg is ObjectDB zNetScene)
+                {
+                    return zNetScene;
+                }
+            }
+
+            return ObjectDB.instance;
+        }
+
+        private struct ZNetSceneState
         {
             public readonly Dictionary<int, GameObject> namedPrefabs;
             public readonly List<GameObject> prefabs;
 
-            public PrefabInfo(IDictionary<int, GameObject> namedPrefabs, IEnumerable<GameObject> prefabs)
+            public ZNetSceneState(ZNetScene zNetScene)
             {
-                this.namedPrefabs = new Dictionary<int, GameObject>(namedPrefabs);
-                this.prefabs = new List<GameObject>(prefabs);
+                this.namedPrefabs = new Dictionary<int, GameObject>(zNetScene.m_namedPrefabs);
+                this.prefabs = new List<GameObject>(zNetScene.m_prefabs);
+            }
+        }
+
+        private struct ObjectDBState
+        {
+            public List<GameObject> items;
+            public List<Recipe> recipes;
+            public Dictionary<int, GameObject> itemByHash;
+
+            public ObjectDBState(ObjectDB objectDB)
+            {
+                items = new List<GameObject>(objectDB.m_items);
+                recipes = new List<Recipe>(objectDB.m_recipes);
+                itemByHash = new Dictionary<int, GameObject>(objectDB.m_itemByHash);
             }
         }
     }
