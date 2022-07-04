6124974b7633918d57bc7d04b4b66e788e87d727
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2022-07-04 20:41:39 +0200

    docs: cleanup VegetationConfig documentation comments

--- JotunnLib/Configs/VegetationConfig.cs
+++ JotunnLib/Configs/VegetationConfig.cs
@@ -1,4 +1,5 @@
 ﻿using Jotunn.Entities;
+using Jotunn.Managers;
 
 namespace Jotunn.Configs
 {
@@ -9,112 +10,137 @@ namespace Jotunn.Configs
     public class VegetationConfig
     {
         /// <summary>
-        ///     Biome to spawn in, can be bitwise or'ed toghether to allow multiple Biomes (Heightmap.Biome.Meadows | Heightmap.Biome.Forest)
+        ///     Biome to spawn in, multiple Biomes can be allowed with <see cref="ZoneManager.AnyBiomeOf"/>
         /// </summary>
         public Heightmap.Biome Biome { get; set; }
+
         /// <summary>
         ///     BiomeArea to spawn in, defaults to Everything
         /// </summary>
         public Heightmap.BiomeArea BiomeArea { get; set; } = Heightmap.BiomeArea.Everything;
+
         /// <summary>
         ///     Do a check before placing that there is nothing already from layers: "Default", "static_solid", "Default_small", "piece"
         /// </summary>
         public bool BlockCheck { get; set; } = true;
+
         /// <summary>
-        ///     Unlike what the name suggets, Valheim will attempt 50 times the normal amount of placements, almost guaranteeing that everything will be placed
+        ///     Unlike what the name suggests, Valheim will attempt 50 times the normal amount of placements, almost guaranteeing that everything will be placed
         /// </summary>
         public bool ForcePlacement { get; set; }
+
         /// <summary>
         ///     Minimum amount per zone
         /// </summary>
         public float Min { get; set; }
+
         /// <summary>
         ///     Values between 0 - 1 are used as a chance to place
         ///     Values above 1 are used as integer amount of maximum per zone
         /// </summary>
         public float Max { get; set; } = 10;
+
         /// <summary>
         ///     Minimal altitude of the vegetation
         /// </summary>
         public float MinAltitude { get; set; } = -1000;
+
         /// <summary>
         ///     Maximum altitude of the vegetation
         /// </summary>
         public float MaxAltitude { get; set; } = 1000;
+
         /// <summary>
         ///     Minimum ocean depth
         /// </summary>
         public float MinOceanDepth { get; set; }
+
         /// <summary>
         ///     Maximum ocean depth
         /// </summary>
         public float MaxOceanDepth { get; set; }
+
         /// <summary>
         ///     Minimum terrain delta (difference between min and max height) in the circle defined by <see cref="TerrainDeltaRadius"/>
         /// </summary>
         public float MinTerrainDelta { get; set; }
+
         /// <summary>
         ///     Maximum terrain delta (difference between min and max height) in the circle defined by <see cref="TerrainDeltaRadius"/>
         /// </summary>
         public float MaxTerrainDelta { get; set; } = 2;
+
         /// <summary>
         ///     Minimum tilt in degrees
         /// </summary>
         public float MinTilt { get; set; }
+
         /// <summary>
         ///     Maximum tilt in degrees
         /// </summary>
         public float MaxTilt { get; set; } = 90;
+
         /// <summary>
         ///     Enable to check forest thresholds against the forest fractal.
         /// </summary>
         public bool InForest { get; set; }
+
         /// <summary>
-        ///     Minimum value of the forest fractal:
-        ///         0 - 1: inside the forest
-        ///         1: forest edge
+        ///     Minimum value of the forest fractal:<br/>
+        ///         0 - 1: inside the forest<br/>
+        ///         1: forest edge<br/>
         ///         1 - infinity: outside the forest
         /// </summary>
         public float ForestThresholdMin { get; set; }
+
         /// <summary>
-        ///     Maximum value of the forest fractal:
-        ///         0 - 1: inside the forest
-        ///         1: forest edge
+        ///     Maximum value of the forest fractal:<br/>
+        ///         0 - 1: inside the forest<br/>
+        ///         1: forest edge<br/>
         ///         1 - infinity: outside the forest
         /// </summary>
         public float ForestThresholdMax { get; set; } = 1;
+
         /// <summary>
         ///     Size of the circle used to determine terrain delta
         /// </summary>
         public float TerrainDeltaRadius { get; set; }
+
         /// <summary>
         ///     Minimum scale of placed instances
         /// </summary>
         public float ScaleMin { get; set; } = 1;
+
         /// <summary>
         ///     Maximum scale of place instances
         /// </summary>
         public float ScaleMax { get; set; } = 1;
+
         /// <summary>
         ///     Minimum amount in group
         /// </summary>
         public int GroupSizeMin { get; set; } = 1;
+
         /// <summary>
         ///     Maximum amount in group
         /// </summary>
         public int GroupSizeMax { get; set; } = 1;
+
         /// <summary>
         ///     Radius of group
         /// </summary>
         public float GroupRadius { get; set; }
+
         /// <summary>
         ///     Placement offset, use negatives to bury underground
         /// </summary>
         public float GroundOffset { get; set; }
+
         /// <summary>
         ///     Create a new <see cref="VegetationConfig"/>
         /// </summary>
         public VegetationConfig() { }
+
         /// <summary>
         ///     Create a copy of the <see cref="ZoneSystem.ZoneVegetation"/>
         /// </summary>
