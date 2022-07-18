bdf61007016833e1a64408e5bed49969e6d6a9c7
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2022-07-18 15:53:27 +0200

    chore: moved inner class of MockExtension into own files

---
+++ JotunnLib/Managers/MockSystem/ClassMember.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Jotunn.Utils;
+
+namespace Jotunn {
+    internal class ClassMember
+    {
+        public readonly List<MemberBase> members = new List<MemberBase>();
+
+        private static readonly Dictionary<Type, ClassMember> CachedClassMembers = new Dictionary<Type, ClassMember>();
+
+        private ClassMember(IEnumerable<FieldInfo> fieldInfos, IEnumerable<PropertyInfo> propertyInfos)
+        {
+            foreach (var fieldInfo in fieldInfos)
+            {
+                members.Add(new FieldMember(fieldInfo));
+            }
+
+            foreach (var propertyInfo in propertyInfos)
+            {
+                members.Add(new PropertyMember(propertyInfo));
+            }
+        }
+
+        private static T[] GetMembersFromType<T>(Type type, Func<Type, T[]> getMembers)
+        {
+            var members = getMembers(type);
+            var baseType = type.BaseType;
+
+            while (baseType != null)
+            {
+                var parentMembers = getMembers(baseType);
+                members = members.Union(parentMembers).ToArray();
+                baseType = baseType.BaseType;
+            }
+
+            return members;
+        }
+
+        public static ClassMember GetClassMember(Type type)
+        {
+            if (CachedClassMembers.TryGetValue(type, out ClassMember classMember))
+            {
+                return classMember;
+            }
+
+            const BindingFlags flags = ReflectionHelper.AllBindingFlags & ~BindingFlags.Static;
+            var fields = GetMembersFromType(type, t => t.GetFields(flags));
+            var properties = GetMembersFromType(type, t => t.GetProperties(flags));
+
+            classMember = new ClassMember(fields, properties);
+            CachedClassMembers[type] = classMember;
+            return classMember;
+        }
+    }
+}
---
+++ JotunnLib/Managers/MockSystem/FieldMember.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Jotunn.Utils;
+using UnityEngine;
+
+namespace Jotunn {
+    internal class FieldMember : MemberBase
+    {
+        private readonly FieldInfo fieldInfo;
+
+        public FieldMember(FieldInfo fieldInfo)
+        {
+            this.fieldInfo = fieldInfo;
+            MemberType = fieldInfo.FieldType;
+            IsUnityObject = MemberType.IsSameOrSubclass(typeof(Object));
+            IsClass = MemberType.IsClass;
+            HasGetMethod = true;
+            EnumeratedType = MemberType.GetEnumeratedType();
+            IsEnumerableOfUnityObjects = EnumeratedType?.IsSameOrSubclass(typeof(Object)) == true;
+            IsEnumeratedClass = EnumeratedType?.IsClass == true;
+        }
+
+        public override object GetValue(object obj)
+        {
+            return fieldInfo.GetValue(obj);
+        }
+
+        public override void SetValue(object obj, object value)
+        {
+            fieldInfo.SetValue(obj, value);
+        }
+    }
+}
