987ed5276d001a3ce572bfafc9e8a113e6cb9e29
Jules <github@sirskunkalot.de>
2022-08-19 22:30:43 +0200

    chore: remove item extension

--- JotunnLib/Extensions/ItemExtensions.cs
+++ JotunnLib/Extensions/ItemExtensions.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using Jotunn.Utils;
 using UnityEngine;
 
 namespace Jotunn
@@ -55,95 +49,6 @@ namespace Jotunn
         /// <param name="self"></param>
         /// <returns></returns>
         public static string TokenName(this ItemDrop self) => self.m_itemData.m_shared.m_name;
-    }
-
-    /// <summary>
-    ///     Extends ItemData with a TokenName.
-    /// </summary>
-    public static class ItemDataExtension
-    {
-        /// <summary>
-        ///     m_shared.m_name
-        /// </summary>
-        /// <param name="self"></param>
-        /// <returns></returns>
-        public static string TokenName(this ItemDrop.ItemData self) => self.m_shared.m_name;
-
-        internal const int LinesToNextEntry = 9;
-
-        internal static string GetUID(this ItemDrop.ItemData self)
-        {
-            var stringBuilder = new StringBuilder();
-
-            stringBuilder.AppendLine(self.m_dropPrefab.name);
-            stringBuilder.AppendLine(self.m_stack.ToString(CultureInfo.InvariantCulture));
-            stringBuilder.AppendLine(self.m_durability.ToString(CultureInfo.InvariantCulture));
-            stringBuilder.AppendLine(self.m_equiped.ToString(CultureInfo.InvariantCulture));
-            stringBuilder.AppendLine(self.m_quality.ToString(CultureInfo.InvariantCulture));
-            stringBuilder.AppendLine(self.m_variant.ToString(CultureInfo.InvariantCulture));
-            stringBuilder.AppendLine(self.m_crafterID.ToString(CultureInfo.InvariantCulture));
-            stringBuilder.AppendLine(self.m_crafterName);
-
-            return HashUtils.ComputeSha256Hash(stringBuilder.ToString());
-        }
-
-        internal static bool RemoveItemFromFile(this ItemDrop.ItemData self, string inventoryId)
-        {
-            var inventoryFilePath = Path.Combine(Paths.CustomItemDataFolder, inventoryId);
-
-            if (File.Exists(inventoryFilePath))
-            {
-                var fileLines = File.ReadAllLines(inventoryFilePath).ToList();
-
-                var itemName = self.m_dropPrefab.name;
-
-                var linesToRemove = new List<int>();
-
-                for (var i = 0; i < fileLines.Count; i += LinesToNextEntry)
-                {
-                    if (fileLines[i] == itemName)
-                    {
-                        linesToRemove.Add(i);
-
-                        break;
-                    }
-                }
-
-                if (linesToRemove.Count > 0)
-                {
-                    foreach (var lineToRemove in linesToRemove)
-                    {
-                        fileLines.RemoveRange(lineToRemove, LinesToNextEntry);
-                    }
-
-                    File.WriteAllLines(inventoryFilePath, fileLines);
-
-                    return true;
-                }
-
-                return false;
-            }
-
-            return true;
-        }
-
-        internal static void SaveToFile(this ItemDrop.ItemData self, string inventoryId)
-        {
-            var stringBuilder = new StringBuilder();
-
-            stringBuilder.AppendLine(self.m_dropPrefab.name);
-            stringBuilder.AppendLine(self.m_stack.ToString(CultureInfo.InvariantCulture));
-            stringBuilder.AppendLine(self.m_durability.ToString(CultureInfo.InvariantCulture));
-            stringBuilder.AppendLine(self.m_equiped.ToString(CultureInfo.InvariantCulture));
-            stringBuilder.AppendLine(self.m_quality.ToString(CultureInfo.InvariantCulture));
-            stringBuilder.AppendLine(self.m_variant.ToString(CultureInfo.InvariantCulture));
-            stringBuilder.AppendLine(self.m_crafterID.ToString(CultureInfo.InvariantCulture));
-            stringBuilder.AppendLine(self.m_crafterName);
-            stringBuilder.AppendLine(self.GetUID());
-
-            var inventoryFilePath = Path.Combine(Paths.CustomItemDataFolder, inventoryId);
-            File.AppendAllText(inventoryFilePath, stringBuilder.ToString());
-        }
     }
 
     /// <summary>
