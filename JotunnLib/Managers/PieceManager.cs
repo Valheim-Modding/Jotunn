6ff19ea06feb3c528dcc3c809955b83145202f13
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-05-14 09:24:21 +0200

    fix: set useCategories only to false if Misc is the only category

--- JotunnLib/Managers/PieceManager.cs
+++ JotunnLib/Managers/PieceManager.cs
@@ -842,7 +842,8 @@ namespace Jotunn.Managers
 
             HashSet<Piece.PieceCategory> visibleCategories = CategoriesInPieceTable(pieceTable);
 
-            pieceTable.m_useCategories = visibleCategories.Count > 1;
+            bool onlyMiscActive = visibleCategories.Count == 1 && visibleCategories.First() == Piece.PieceCategory.Misc;
+            pieceTable.m_useCategories = !onlyMiscActive;
 
             int originallyVisibleTabs = 0;
             int visibleTabs = 0;
