bec5897b66564b8a01e86c5c0aa2dbf831833237
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-10-24 15:33:09 +0200

    chore: moved PatchInit from Main to own class

--- JotunnLib/Main.cs
+++ JotunnLib/Main.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using BepInEx;
 using BepInEx.Bootstrap;
 using HarmonyLib;
@@ -64,7 +61,7 @@ namespace Jotunn
 #endif
 
 #pragma warning disable CS0612 // Method is obsolete
-            InitializePatches();
+            PatchInit.InitializePatches();
 #pragma warning restore CS0612 // Method is obsolete
 
             AutomaticLocalizationsLoading.Init();
@@ -87,59 +84,6 @@ namespace Jotunn
             rootObject = new GameObject("_JotunnRoot");
             DontDestroyOnLoad(rootObject);
             return rootObject;
-        }
-
-        /// <summary>
-        ///     Invoke patch initialization methods for all loaded mods.
-        /// </summary>
-        [Obsolete]
-        private void InitializePatches()
-        {
-            List<Tuple<MethodInfo, int>> types = new List<Tuple<MethodInfo, int>>();
-            HashSet<Assembly> searchedAssemblies = new HashSet<Assembly>();
-
-            // Check in all Jotunn mods
-            foreach (var baseUnityPlugin in BepInExUtils.GetDependentPlugins().Values)
-            {
-                try
-                {
-                    Assembly asm = baseUnityPlugin.GetType().Assembly;
-
-                    // Skip already searched assemblies
-                    if (searchedAssemblies.Contains(asm))
-                    {
-                        continue;
-                    }
-
-                    searchedAssemblies.Add(asm);
-
-                    // Search in all types
-                    foreach (var type in asm.GetTypes())
-                    {
-                        try
-                        {
-                            // on methods with the PatchInit attribute
-                            foreach (var method in type.GetMethods(BindingFlags.Static | BindingFlags.Public)
-                                .Where(x => x.GetCustomAttributes(typeof(PatchInitAttribute), false).Length == 1))
-                            {
-                                var attribute = method.GetCustomAttributes(typeof(PatchInitAttribute), false).FirstOrDefault() as PatchInitAttribute;
-                                types.Add(new Tuple<MethodInfo, int>(method, attribute.Priority));
-                            }
-                        }
-                        catch (Exception)
-                        { }
-                    }
-                }
-                catch (Exception)
-                { }
-            }
-
-            // Invoke the method
-            foreach (Tuple<MethodInfo, int> tuple in types.OrderBy(x => x.Item2))
-            {
-                Jotunn.Logger.LogDebug($"Applying patches in {tuple.Item1.DeclaringType.Name}.{tuple.Item1.Name}");
-                tuple.Item1.Invoke(null, null);
-            }
         }
 
         internal static void LogInit(string module)
