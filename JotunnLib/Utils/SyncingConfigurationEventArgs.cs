35099052eeb4b546e661fcc631556034cd9248fd
searica <143636061+searica@users.noreply.github.com>
2023-11-01 09:39:31 -0700

    Added new eventargs class

---
+++ JotunnLib/Utils/SyncingConfigurationEventArgs.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Jotunn.Utils
+{
+    /// <summary>
+    ///     Event args class for syncing configuration event
+    /// </summary>
+    public class SyncingConfigurationEventArgs : EventArgs
+    {
+        // currently empty but thought that keeping
+        // symmetry with ConfigurationSynchronizationEventArgs was
+        // a better design and that other devs might have
+        // ideas of what should go here so doing it this way
+        // was more future-proof.
+    }
+}
