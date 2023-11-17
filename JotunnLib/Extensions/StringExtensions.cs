923b82f652b6d240d3b590622db07953052e4912
searica <143636061+searica@users.noreply.github.com>
2023-11-17 13:02:11 -0800

    Add string extensions for handling substrings, prefixes, and suffixes

---
+++ JotunnLib/Extensions/StringExtensions.cs
@@ -0,0 +1,92 @@
+namespace Jotunn.Extensions
+{
+    internal static class StringExtensions
+    {
+        /// <summary>
+        ///     Returns true if the string contains any of the substrings.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="substrings"></param>
+        /// <returns></returns>
+        internal static bool ContainsAny(this string str, params string[] substrings)
+        {
+            foreach (var substring in substrings)
+            {
+                if (str.Contains(substring))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///     Returns true if the string ends with any one of the suffixes.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="suffixes"></param>
+        /// <returns></returns>
+        internal static bool EndsWithAny(this string str, params string[] suffixes)
+        {
+            foreach (var substring in suffixes)
+            {
+                if (str.EndsWith(substring))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///     Returns true if the string starts with any one of the prefixes.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="prefixes"></param>
+        /// <returns></returns>
+        internal static bool StartsWithAny(this string str, params string[] prefixes)
+        {
+            foreach (var substring in prefixes)
+            {
+                if (str.StartsWith(substring))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///     If the string ends with the suffix then return a copy of the string
+        ///     with the suffix stripped, otherwise return the original string.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="suffix"></param>
+        /// <returns></returns>
+        internal static string RemoveSuffix(this string s, string suffix)
+        {
+            if (s.EndsWith(suffix))
+            {
+                return s.Substring(0, s.Length - suffix.Length);
+            }
+
+            return s;
+        }
+
+        /// <summary>
+        ///     If the string starts with the prefix then return a copy of the string
+        ///     with the prefix stripped, otherwise return the original string.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
+        internal static string RemovePrefix(this string s, string prefix)
+        {
+            if (s.StartsWith(prefix))
+            {
+                return s.Substring(prefix.Length, s.Length - prefix.Length);
+            }
+            return s;
+        }
+    }
+}
