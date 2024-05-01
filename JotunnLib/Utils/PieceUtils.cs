a9888c9131c23138a3997a5a61adb2210220a429
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2024-05-01 17:54:43 +0200

    chore: simplefied parsing of vanilla max category

--- JotunnLib/Utils/PieceUtils.cs
+++ JotunnLib/Utils/PieceUtils.cs
@@ -5,20 +5,19 @@ namespace Jotunn.Utils
 {
     internal static class PieceUtils
     {
-        public static int VanillaMaxPieceCategory { get; } = GetMaxPieceCategory();
+        public static int VanillaMaxPieceCategory { get; } = GetVanillaMaxPieceCategory();
 
-        private static int GetMaxPieceCategory()
+        private static int GetVanillaMaxPieceCategory()
         {
-            var indexOfMax = Array.IndexOf(Enum.GetNames(typeof(Piece.PieceCategory)), nameof(Piece.PieceCategory.Max));
-            var values = Enum.GetValues(typeof(Piece.PieceCategory)).Cast<int>().ToArray();
-
-            if (indexOfMax >= 0)
+            try
             {
-                return values[indexOfMax];
+                return (int)Enum.Parse(typeof(Piece.PieceCategory), nameof(Piece.PieceCategory.Max));
             }
-
-            Logger.LogWarning("Could not find Piece.PieceCategory.Max, using fallback value 4");
-            return 4;
+            catch (Exception e)
+            {
+                Logger.LogWarning("Could not find Piece.PieceCategory.Max, using fallback value 4");
+                return 4;
+            }
         }
     }
 }
