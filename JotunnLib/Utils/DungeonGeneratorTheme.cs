3c2202f75cc22a4cc6265d0d6113873676cf30f7
jneb802 <73610029+jneb802@users.noreply.github.com>
2024-07-17 11:12:15 -0700

    Initial checkin of (kory) DungeonManager - kory commit

---
+++ JotunnLib/Utils/DungeonGeneratorTheme.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Jotunn.Utils
+{
+    /// <summary>
+    ///     Helper class for identifying the theme of custom rooms.
+    /// </summary>
+    public class DungeonGeneratorTheme : MonoBehaviour
+    {
+        /// <summary>
+        ///     Name of a dungeon theme, used when matching generators with custom rooms.
+        /// </summary>
+        public string m_themeName = "";
+    }
+}
