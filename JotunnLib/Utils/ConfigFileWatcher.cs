2b1eb32bb5234fcaaaa718ef0459d8a6a0f1cbf1
Searica <143636061+searica@users.noreply.github.com>
2025-03-08 09:57:18 -0800

    Fix: Remove exposing user info on config reload

--- JotunnLib/Utils/ConfigFileWatcher.cs
+++ JotunnLib/Utils/ConfigFileWatcher.cs
@@ -75,9 +75,12 @@ namespace Jotunn.Utils
                 return;
             }
 
+            // Only log file name to avoid exposing user info if it located within AppData (such as when using r2modman)
+            string configFileName = Path.GetFileName(configFile.ConfigFilePath);
             try
             {
-                Logger.LogInfo(sourceMod, $"Reloading {configFile.ConfigFilePath}");
+                
+                Logger.LogInfo(sourceMod, $"Reloading {configFileName}");
                 bool saveOnConfigSet = configFile.SetSaveOnConfigSet(false); // turn off saving on config entry set
                 configFile.Reload();
                 configFile.SaveOnConfigSet = saveOnConfigSet; // reset config saving state
@@ -86,7 +89,7 @@ namespace Jotunn.Utils
             }
             catch
             {
-                Logger.LogError(sourceMod, $"There was an issue loading {configFile.ConfigFilePath}");
+                Logger.LogError(sourceMod, $"There was an issue loading {configFileName}");
                 Logger.LogError(sourceMod, "Please check your config entries for spelling and format!");
             }
         }
